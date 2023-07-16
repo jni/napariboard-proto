6b8cd7689126e6133e0e55b995019240a6c74a4f
Juan Nunez-Iglesias <jni@fastmail.com>
2023-07-16 10:27:10 -0500

    Update for napari 0.4.18

--- napariboard.py
+++ napariboard.py
@@ -129,96 +129,99 @@ model_output_dask = da.map_blocks(
 
 # set the dask cache size to 0 because we want the output to be recomputed each
 # time we call it, since the tensor model is training and will have changed.
+import napari.settings
+settings = napari.settings.get_settings()
+
+settings.application.dask.enabled = False
+
+
 import napari
-from napari.utils import resize_dask_cache
-resize_dask_cache(0)
+viewer = napari.Viewer()
+# uncomment the line below if you want to see the ground truth,
+# which is not actually used by the model.
+# _ = viewer.add_image(clean_test)  # returns layer, we don't care
+_ = viewer.add_image(noisy_test)  # returns layer, we don't care
+model_layer = viewer.add_image(
+    model_output_dask,
+    colormap='magma',
+)  # this layer though, we're gonna play with
+viewer.grid.enabled = True
 
-with napari.gui_qt():
-    viewer = napari.Viewer()
-    # uncomment the line below if you want to see the ground truth,
-    # which is not actually used by the model.
-    # _ = viewer.add_image(clean_test)  # returns layer, we don't care
-    _ = viewer.add_image(noisy_test)  # returns layer, we don't care
-    model_layer = viewer.add_image(
-        model_output_dask,
-        colormap='magma',
-    )  # this layer though, we're gonna play with
-    viewer.grid.enabled = True
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+from matplotlib.figure import Figure
 
-    import matplotlib.pyplot as plt
-    from matplotlib.backends.backend_qt5agg import FigureCanvas
-    from matplotlib.figure import Figure
+NUM_ITER = 500
 
-    NUM_ITER = 500
+# build the plot, but don't display it yet
+# — we'll add it to the napari viewer later
+with plt.style.context('dark_background'):
+    loss_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+    loss_axes = loss_canvas.figure.subplots()
+    lines = loss_axes.plot([], [])  # make empty plot
+    loss_axes.set_xlim(0, NUM_ITER)
+    loss_axes.set_xlabel('batch number')
+    loss_axes.set_ylabel('loss')
+    loss_canvas.figure.tight_layout()
+    loss_line = lines[0]
 
-    # build the plot, but don't display it yet
-    # — we'll add it to the napari viewer later
-    with plt.style.context('dark_background'):
-        loss_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-        loss_axes = loss_canvas.figure.subplots()
-        lines = loss_axes.plot([], [])  # make empty plot
-        loss_axes.set_xlim(0, NUM_ITER)
-        loss_axes.set_xlabel('batch number')
-        loss_axes.set_ylabel('loss')
-        loss_canvas.figure.tight_layout()
-        loss_line = lines[0]
+# Napari's threading utilities, created by Talley Lambert, allow *yielding*
+# of values during a thread's execution, and connecting those yielded
+# values to callbacks. Below, we create callbacks to update the loss plot
+# and the displayed model output:
 
-    # Napari's threading utilities, created by Talley Lambert, allow *yielding*
-    # of values during a thread's execution, and connecting those yielded
-    # values to callbacks. Below, we create callbacks to update the loss plot
-    # and the displayed model output:
-
-    # when getting a new loss, update the plot
-    def update_plot(loss):
-        x, y = loss_line.get_data()
-        new_y = np.append(y, loss)
-        new_x = np.arange(len(new_y))
-        loss_line.set_data(new_x, new_y)
-        loss_axes.set_ylim(
-            np.min(new_y) * (-0.05), np.max(new_y) * (1.05)
-        )
-        loss_canvas.draw_idle()
+# when getting a new loss, update the plot
+def update_plot(loss):
+    x, y = loss_line.get_data()
+    new_y = np.append(y, loss)
+    new_x = np.arange(len(new_y))
+    loss_line.set_data(new_x, new_y)
+    loss_axes.set_ylim(
+        np.min(new_y) * (-0.05), np.max(new_y) * (1.05)
+    )
+    loss_canvas.draw_idle()
 
 
-    # and update the model output layer
-    def update_viewer(loss):
-        model_layer.refresh()
-        model_layer._set_view_slice()
-        viewer.help = f'loss: {loss}'
+# and update the model output layer
+def update_viewer(loss):
+    model_layer.refresh()
+    model_layer._set_view_slice()
+    viewer.help = f'loss: {loss}'
 
 
-    # We then do two things to a standard PyTorch training loop:
-    # 
-    # - we wrap it in a function with the `@thread_worker` decorator
-    # - inside the function, we yield the loss value after each training batch
+# We then do two things to a standard PyTorch training loop:
+# - we wrap it in a function with the `@thread_worker` decorator
+# - inside the function, we yield the loss value after each training batch
 
-    from napari.qt import thread_worker
+from napari.qt import thread_worker
 
-    # define a function to train the model in a new thread,
-    # connecting the yielded loss values to our update functions
-    @thread_worker(connect={'yielded': [update_viewer, update_plot]})
-    def train(model, data_loader, n_iter):
+# define a function to train the model in a new thread,
+# connecting the yielded loss values to our update functions
+@thread_worker(connect={'yielded': [update_viewer, update_plot]})
+def train(model, data_loader, n_iter):
 
-        for i, batch in zip(range(n_iter), data_loader):
-            noisy_images, clean_images = batch
+    for i, batch in zip(range(n_iter), data_loader):
+        noisy_images, clean_images = batch
 
-            net_input, mask = masker.mask(noisy_images, i)
-            net_output = model(net_input)
+        net_input, mask = masker.mask(noisy_images, i)
+        net_output = model(net_input)
 
-            loss = loss_function(net_output*mask, noisy_images*mask)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        loss = loss_function(net_output*mask, noisy_images*mask)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-            yield round(loss.item(), 4)
+        yield round(loss.item(), 4)
 
 
-    # Finally, we create the PyTorch DataLoader, add the loss plot to our
-    # viewer, and start training! You should be able to see the model output
-    # refine over time, while simultaneously browsing through the whole test
-    # dataset
+# Finally, we create the PyTorch DataLoader, add the loss plot to our
+# viewer, and start training! You should be able to see the model output
+# refine over time, while simultaneously browsing through the whole test
+# dataset
 
-    data_loader = DataLoader(noisy_mnist_train, batch_size=32, shuffle=True)
+data_loader = DataLoader(noisy_mnist_train, batch_size=32, shuffle=True)
 
-    viewer.window.add_dock_widget(loss_canvas)
-    worker = train(model, data_loader, NUM_ITER)
+viewer.window.add_dock_widget(loss_canvas, area='bottom')
+worker = train(model, data_loader, NUM_ITER)
+
+napari.run()
